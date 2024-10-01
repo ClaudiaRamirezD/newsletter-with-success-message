a934bd0b8cf5f552f6152b222d38eb3eb3ab834b
Claudia Ramirez <clau8812@gmail.com>
2024-10-01 16:30:03 -0600

    Form with js

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const subscribeButton = document.querySelector(".button");
 const dismissButton = document.querySelector(".dismiss-button");
 const subscribePage = document.querySelector(".subscribe-page");
 const containerSuccess = document.querySelector(".container-success");
+const emailInput = document.querySelector("#emailInput");
+const emailDisplay = document.querySelector(".emailDisplay");
+const errorMessage = document.querySelector(".error");
+
+// Email validation regex
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 // Function to hide subscribe page and show success message
 subscribeButton.addEventListener("click", (e) => {
     e.preventDefault(); // Prevent form submission
-    subscribePage.style.display = "none"; // Hide the subscribe page
-    containerSuccess.style.display = "flex"; // Show the success container
+    const emailValue = emailInput.value;
+
+    if (!emailRegex.test(emailValue)) {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+        //Show success message and display the mail
+        emailDisplay.textContent = emailValue;
+        subscribePage.style.display = "none"; // Hide the subscribe page
+        containerSuccess.style.display = "flex"; // Show the success container
+    }
     e.stopPropagation();
 });
 
